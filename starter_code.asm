a15b4b0824f193f0033235f6d55a8db7fa5b259e
AlvinF0527 <100899710+AlvinF0527@users.noreply.github.com>
2022-03-04 00:46:29 -0800

    Add files via upload

--- starter_code.asm
+++ starter_code.asm
@@ -1,10 +1,10 @@
 .orig x3000
+;this stack lab computes the polish notation of a set of calls
 
 LD R6, TOP_STACK
 LD R5, MAX_STACK
 LD R4, BASE_STACK
 
-;this stack lab computes the polish notation of a set of calls
 ;push_val(4) pushes the value 4 onto the stack [4]
 LD R0, DEC_4
 LD R1, PUSH_VAL_SUB
@@ -31,8 +31,6 @@ JSRR R1
 LD R1, ADD_VAL_SUB
 JSRR R1
 
-
-
 ;move the top value of the stack into r4
 LDR R4, R6, #0
 
@@ -54,20 +52,13 @@ DEC_0 .FILL #0
 
 .end
 
-
-
-
-
 .orig x3400 ;;push_val(int val)implement your push function that will push a value onto the stack
 
     ST R7, backup_r7_3400
-;----------------------------------------------------
-
+    
     ADD R6, R6, #1 
     STR R0, R6, #0
-     
     
-;----------------------------------------------------    
     LD R7, backup_r7_3400
     
     
@@ -80,19 +71,15 @@ DEC_0 .FILL #0
 
 
 .orig x3800 ;; add_val() pops two values from the top of the stack and pushes the result of adding the poppped value into the stack
+   
     ST R0, backup_r0_3800
     ST R3, backup_r3_3800
     ST R4, backup_r4_3800
     ST R7, backup_r7_3800
-;----------------------------------------------------
 
     LDR R3, R6, #0       
     STR R0, R6, #0
     ADD R6, R6, #-1
-    
-    
-    
-    
     
     LDR R4, R6, #0            
    ; ADD R6, R6, #-1
@@ -100,20 +87,11 @@ DEC_0 .FILL #0
     ADD R3, R3, R4
     STR R3, R6, #0
     
-    
-
-
-
-;----------------------------------------------------
-
-
     LD R0, backup_r0_3800
     LD R3, backup_r3_3800
     LD R4, backup_r4_3800
     LD R7, backup_r7_3800
-    
-    
-    
+  
     ret
     
     backup_r0_3800 .BLKW  #1  
